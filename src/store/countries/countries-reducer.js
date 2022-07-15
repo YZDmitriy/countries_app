602b73bdf6cf275a80d308cc4168458ff3b60147
Dmitriy <dyuzef@mail.ru>
2022-07-15 13:00:03 +0300

    add action reducer selector for countries

---
+++ src/store/countries/countries-reducer.js
@@ -0,0 +1,33 @@
+import { SET_LOADING, SET_COUNTRIES, SET_ERROR } from './countries-actions';
+
+const initialState = {
+  status: 'idle', // loading | received | rejected
+  error: null,
+  lost: [],
+};
+
+export const countriesReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        status: 'loading',
+        error: null,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        status: 'rejected',
+        error: payload,
+      };
+    case SET_COUNTRIES:
+      return {
+        ...state,
+        status: 'received',
+        list: payload,
+      };
+
+    default:
+      return state;
+  }
+};
---
+++ src/store/countries/countries-selector.js
@@ -0,0 +1,7 @@
+export const selectCounties = (state) => ({
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.list.length,
+});
+
+export const selectAllCountries = (state) => state.countries.list;
